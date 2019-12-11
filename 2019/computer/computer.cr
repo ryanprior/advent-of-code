dfd2a51936fa0c05c095786350c366b2ae74a326
Ryan Prior <ryanprior@gmail.com>
2019-12-11 15:09:21 -0600

    Refactors computer.cr to remove 4-int-per-operation assumption

--- 2019/computer/computer.cr
+++ 2019/computer/computer.cr
@@ -1,11 +1,44 @@
 require "immutable"
 
-def instruction(data, offset)
-  [data[offset*4], data[offset*4 + 1]?, data[offset*4 + 2]?, data[offset*4 + 3]?]
+enum OpCode
+  Add,
+  Multiply,
+  Halt
 end
 
-def halt?(instruction)
-  instruction.first == 99
+record Operation,
+       code : Int32,
+       id : OpCode,
+       num_read_params : Int32,
+       num_write_params : Int32 do
+  def halt?
+    code == 99
+  end
+
+  def size
+    num_read_params + num_write_params + 1
+  end
+end
+
+record Instruction,
+       operation : Operation,
+       read_params : Array(Int32),
+       write_params : Array(Int32) do
+  def self.from(data_in, offset)
+    op = Intcode.operations[data_in[offset]]
+    params = data_in.to_a[offset + 1..offset + op.size].each
+    new operation: op,
+        read_params: params.first(op.num_read_params).to_a,
+        write_params: params.first(op.num_write_params).to_a
+  end
+end
+
+module Intcode
+  def self.operations
+    {1  => Operation.new(code: 1,  id: OpCode::Add, num_read_params: 2, num_write_params: 1),
+     2  => Operation.new(code: 2,  id: OpCode::Multiply, num_read_params: 2, num_write_params: 1),
+     99 => Operation.new(code: 99, id: OpCode::Halt, num_read_params: 0, num_write_params: 0)}
+  end
 end
 
 def panic(message, exit_code = 1)
@@ -14,31 +47,29 @@ def panic(message, exit_code = 1)
 end
 
 def apply(instruction, data)
-  opcode, register_a, register_b, destination = instruction
-  return data if !register_a
-  register_a = register_a || 0
-  register_b = register_b || 0
-  destination = destination || 0
-  data.set(destination, case opcode
-                        when 1
-                          data[register_a] + data[register_b]
-                        when 2
-                          data[register_a] * data[register_b]
-                        else
-                          panic "fatal: unrecognized opcode #{opcode}"
-                        end)
+  case instruction.operation.id
+  when OpCode::Add
+    a, b = instruction.read_params.map { |param| data[param] }
+    data.set(instruction.write_params.first, a + b)
+  when OpCode::Multiply
+    a, b = instruction.read_params.map { |param| data[param] }
+    data.set(instruction.write_params.first, a * b)
+  when OpCode::Halt
+    data
+  else
+    panic "fatal: unrecognized opcode #{instruction.operation.code}"
+  end
 end
 
 def run(data)
   offset = 0
   loop do
-    panic "warning: no more instructions, halting" if offset*4 > data.size
-    next_instruction = instruction(data, offset)
-    break if halt? next_instruction
+    panic "fatal: no more instructions" if offset > data.size
+    next_instruction = Instruction.from(data, offset)
+    break data if next_instruction.operation.halt?
+    offset += next_instruction.operation.size
     data = apply(next_instruction, data)
-    offset += 1
   end
-  data
 end
 
 def format_program(data, use_newline = false)
