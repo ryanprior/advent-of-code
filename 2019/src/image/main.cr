81f398390ede86e334f51392fc90376a055ca679
Ryan Prior <ryanprior@gmail.com>
2019-12-18 14:33:58 -0600

    Adds help to `image` program

--- 2019/src/image/main.cr
+++ 2019/src/image/main.cr
@@ -7,6 +7,7 @@ class ImageCommand < Cli::Command
            desc: "dimensions of input image in pixels (eg \"3x5\")",
            required: true
     arg "file", desc: "image file to read (in Space Image Format)"
+    help
   end
 
   def dimensions
@@ -16,6 +17,11 @@ end
 
 class Image < Cli::Supercommand
   class Check < ImageCommand
+    class Help
+      header "Summarizes the content of an image (SIF) for error checking purposes"
+      footer "This tool is part of Ryan Prior's 2019 Advent of Code"
+    end
+
     def run
       input = File.read(args.file).strip
       counts = layers(input, dimensions).map(&.tally)
@@ -25,6 +31,11 @@ class Image < Cli::Supercommand
   end
 
   class Show < ImageCommand
+    class Help
+      header "Prints an image (SIF) to the console"
+      footer "This tool is part of Ryan Prior's 2019 Advent of Code"
+    end
+
     def run
       input = File.read(args.file).strip
       puts format_sif(assemble(layers(input, dimensions)), dimensions.first)
